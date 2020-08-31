868549fdfc844a364ac7093e724e0201a68653c5
ikhwanisbatullah <ikhwanisbat@gmail.com>
2020-08-31 13:44:44 +0700

    Upload foto dll

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,10 +7,23 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
-    public function index()
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
 	{
-        
+		parent::__construct();
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
 		return view('admin.dashboard.index', $this->data);
 	}
 }
